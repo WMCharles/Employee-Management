48c32dafacbe2d2c5614787cfc42a820862deec8
wmcharles <charleswafula73@gmail.com>
2023-03-03 13:32:41 +0300

    fetch data

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Employee Management</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+   
+
+    <!-- Add Employee Modal -->
+    <div class="modal fade" id="addEmployeeModal" tabindex="-1" role="dialog" aria-labelledby="addEmployeeModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="addEmployeeModalLabel">Add Employee</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form id="addEmployeeForm">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="name" name="name" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="department">Department</label>
+                            <select class="form-control" id="department" name="department" required>
+                                <option value="">Select Department</option>
+                            </select>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" form="addEmployeeForm" class="btn btn-primary">Add Employee</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Edit Employee Modal -->
+    <div class="modal fade" id="editEmployeeModal" tabindex="-1" role="dialog" aria-labelledby="editEmployeeModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editEmployeeModalLabel">Edit Employee</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form id="editEmployeeForm">
+                        <input type="hidden" id="editEmployeeId" name="id">
+                        <div class="form-group">
+                            <label for="editName">Name</label>
+                            <input type="text" class="form-control" id="editName" name="name" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="editEmail">Email</label>
+                            <input type="email" class="form-control" id="editEmail" name="email" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="editDepartment">Department</label>
+                            <select class="form-control" id="editDepartment" name="department" required>
+                                <option value="">Select Department</option>
+                            </select>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" form="editEmployeeForm" class="btn btn-primary">Save Changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-4">
+        <h1 class="text-center mb-4">Employee Management</h1>
+        <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#addEmployeeModal">Add Employee</button>
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered" id="employeeTable">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Department</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    // Connect to database
+                    $conn = new mysqli("localhost", "root", "", "bee_farm");
+
+                    // Check connection
+                    if ($conn->connect_errno) {
+                        echo "Failed to connect to MySQL: " . $conn->connect_error;
+                        exit();
+                    }
+
+                    // Query to retrieve employees with department names
+                    $employeesQuery = "SELECT employees.id, employees.name, employees.email, departments.name AS department_name FROM employees LEFT JOIN departments ON employees.department_id = departments.id";
+
+                    // Execute query
+                    $employeesResult = $conn->query($employeesQuery);
+
+                    // Loop through employees and create table rows
+                    if ($employeesResult->num_rows > 0) {
+                        while ($employee = $employeesResult->fetch_assoc()) {
+                            echo "<tr>";
+                            echo "<td>" . $employee["id"] . "</td>";
+                            echo "<td>" . $employee["name"] . "</td>";
+                            echo "<td>" . $employee["email"] . "</td>";
+                            echo "<td>" . $employee["department_name"] . "</td>";
+                            echo "<td><button type=\"button\" class=\"btn btn-primary editEmployeeButton\" data-toggle=\"modal\" data-target=\"#editEmployeeModal\" data-id=\"" . $employee["id"] . "\">Edit</button> <button type=\"button\" class=\"btn btn-danger deleteEmployeeButton\" data-id=\"" . $employee["id"] . "\">Delete</button></td>";
+                            echo "</tr>";
+                        }
+                    }
+                    // Close database connection
+                    $mysqli->close();
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+    <!-- Bootstrap 4 -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.min.js"></script>
+
+    <!-- Custom JavaScript -->
+    <script src="./assets/js/script.js"></script>
+</body>
+
+</html>
