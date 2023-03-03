63af142e3ac06b1f09dc018e2b1f61f03ee7efdc
wmcharles <charleswafula73@gmail.com>
2023-03-03 13:49:34 +0300

    fetch departments

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-   
+
 
     <!-- Add Employee Modal -->
     <div class="modal fade" id="addEmployeeModal" tabindex="-1" role="dialog" aria-labelledby="addEmployeeModalLabel" aria-hidden="true">
@@ -36,6 +36,32 @@
                             <label for="department">Department</label>
                             <select class="form-control" id="department" name="department" required>
                                 <option value="">Select Department</option>
+                                <?php
+                                // Connect to database
+                                $mysqli = new mysqli("localhost", "root", "", "bee_farm");
+
+                                // Check connection
+                                if ($mysqli->connect_errno) {
+                                    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+                                    exit();
+                                }
+
+                                // Query to retrieve departments
+                                $departmentsQuery = "SELECT * FROM departments";
+
+                                // Execute query
+                                $departmentsResult = $mysqli->query($departmentsQuery);
+
+                                // Loop through departments and create options
+                                if ($departmentsResult->num_rows > 0) {
+                                    while ($department = $departmentsResult->fetch_assoc()) {
+                                        echo "<option value=\"" . $department["id"] . "\">" . $department["name"] . "</option>";
+                                    }
+                                }
+
+                                // Close database connection
+                                $mysqli->close();
+                                ?>
                             </select>
                         </div>
                     </form>
@@ -73,6 +99,32 @@
                             <label for="editDepartment">Department</label>
                             <select class="form-control" id="editDepartment" name="department" required>
                                 <option value="">Select Department</option>
+                                <?php
+                                // Connect to database
+                                $mysqli = new mysqli("localhost", "root", "", "bee_farm");
+
+                                // Check connection
+                                if ($mysqli->connect_errno) {
+                                    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+                                    exit();
+                                }
+
+                                // Query to retrieve departments
+                                $departmentsQuery = "SELECT * FROM departments";
+
+                                // Execute query
+                                $departmentsResult = $mysqli->query($departmentsQuery);
+
+                                // Loop through departments and create options
+                                if ($departmentsResult->num_rows > 0) {
+                                    while ($department = $departmentsResult->fetch_assoc()) {
+                                        echo "<option value=\"" . $department["id"] . "\">" . $department["name"] . "</option>";
+                                    }
+                                }
+
+                                // Close database connection
+                                $mysqli->close();
+                                ?>
                             </select>
                         </div>
                     </form>
